8c0941e6251e766a653abd50a376b87f3a38fc7d
galih <samudragalih94@gmail.com>
2023-07-27 13:38:43 +0700

    feat(LandmarkRow): Create the Row View

---
+++ Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,25 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Galih Samudra on 27/07/23.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    var body: some View {
+        HStack {
+            landmark.image.resizable().frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }.padding()
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarks[0])
+    }
+}
