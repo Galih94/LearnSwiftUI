dd0741b6ab6f2b8c1c90b3fb7d6b9a90d00952c7
galih <samudragalih94@gmail.com>
2023-07-31 15:08:42 +0700

    feat(PageControl): Add a Custom Page Control

---
+++ Landmarks/Landmarks/Views/PageView/PageControl.swift
@@ -0,0 +1,45 @@
+//
+//  PageControl.swift
+//  Landmarks
+//
+//  Created by Galih Samudra on 31/07/23.
+//
+
+import SwiftUI
+import UIKit
+
+struct PageControl: UIViewRepresentable {
+    
+    var numberOfPages: Int
+    @Binding var currentPage: Int
+    
+    func makeUIView(context: Context) -> UIPageControl {
+        let control = UIPageControl()
+        control.numberOfPages = numberOfPages
+        control.addTarget(
+            context.coordinator,
+            action: #selector(Coordinator.updateCurrentPage(sender:)),
+            for: .valueChanged)
+        
+        return control
+    }
+    func updateUIView(_ uiView: UIPageControl, context: Context) {
+        uiView.currentPage = currentPage
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(control: self)
+    }
+    
+    class Coordinator: NSObject {
+        var control: PageControl
+        init(control: PageControl) {
+            self.control = control
+        }
+        
+        @objc
+        func updateCurrentPage(sender: UIPageControl) {
+            control.currentPage = sender.currentPage
+        }
+    }
+}
