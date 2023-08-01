2231e3c8563d75cb06b49fb64ab4a552c72867dc
galih <samudragalih94@gmail.com>
2023-08-01 13:05:01 +0700

    feat(LandmarkCommands): Add a Built-in Menu Command

---
+++ Landmarks/MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,12 @@
+//
+//  LandmarkCommands.swift
+//  MacLandmarks
+//
+//  Created by Galih Samudra on 01/08/23.
+//
+import SwiftUI
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
